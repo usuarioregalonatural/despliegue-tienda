aba129ba24329874253d4cf0ea9835645651ab68
usuarioregalonatural <webmaster@regalonatural.com>
2020-01-25 20:22:23 +0100

    actualizaciones-20200125

--- 1200-backup-sql.sh
+++ 1200-backup-sql.sh
@@ -5,7 +5,7 @@
         source ./00000-entorno.cfg
 
 	# hago backup del fichero sql
-	mv ${PROD_RUTA_WEB}/bck_regalonatural.sql ${PROD_RUTA_WEB}/bck_regalonatural.sql.${ID_UNICO_PROCESO}
+	mv ${PROD_RUTA_WEB}/bck_regalonatural.sql ${PROD_RUTA_WEB}/backup-sql/bck_regalonatural.sql.${ID_UNICO_PROCESO}
 	docker exec ${VAR_CONT_MYSQL} /usr/bin/mysqldump -u root --password=vmsn2004 regalonatural > ${PROD_RUTA_WEB}/bck_regalonatural.sql
 
 
